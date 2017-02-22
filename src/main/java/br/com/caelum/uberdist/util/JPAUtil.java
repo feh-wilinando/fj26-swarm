fcd1342d8ba66a9d773c7cd298fe16c113871064
Fernando Furtado <feh.wilinando@gmail.com>
2017-02-21 22:57:19 -0300

    Agora sim externalizando de forma correta, pegando parâmetros de conexão a partir de váriaveis de ambiente

--- src/main/java/br/com/caelum/uberdist/util/JPAUtil.java
+++ src/main/java/br/com/caelum/uberdist/util/JPAUtil.java
@@ -1,5 +1,6 @@
 package br.com.caelum.uberdist.util;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
@@ -7,6 +8,8 @@ import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by nando on 13/02/17.
@@ -14,8 +17,31 @@ import javax.persistence.Persistence;
 @ApplicationScoped
 public class JPAUtil {
 
-    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("notas");
+    private static EntityManagerFactory factory;
 
+
+    @PostConstruct
+    public void onLoad(){
+
+        Map<String, String> properties = new HashMap<>();
+        Map<String, String> env = System.getenv();
+
+        String dbHost = env.getOrDefault("DB_HOST", "localhost");
+        String dbPort = env.getOrDefault("DB_PORT", "3306");
+        String dbName = env.getOrDefault("DB_NAME", "fj26");
+        String dbUser = env.getOrDefault("DB_USER", "root");
+        String dbPassword = env.getOrDefault("DB_PASSWORD", "");
+
+
+        String connectionString = String.format("jdbc:mysql://%s:%s/%s", dbHost, dbPort, dbName);
+
+        properties.put("javax.persistence.jdbc.url", connectionString);
+        properties.put("javax.persistence.jdbc.user", dbUser);
+        properties.put("javax.persistence.jdbc.password", dbPassword);
+
+        factory = Persistence.createEntityManagerFactory("notas", properties);
+
+    }
 
     @Produces
     @RequestScoped
